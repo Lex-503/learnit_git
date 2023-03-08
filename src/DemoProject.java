7c7c55edd328d4d4b098fe167a5e055826873d6d
Lex Rodriguez <alexanderrodriguezz1990@gmail.com>
2023-03-07 23:00:03 -0600

    Personal discount for user can be calculated

--- src/DemoProject.java
+++ src/DemoProject.java
@@ -1,6 +1,7 @@
 
 public class DemoProject {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo Hard!!");
+		System.out.println("Hola Mundo Hard!!!");
+		System.out.println("Prun");
 	}
 }
